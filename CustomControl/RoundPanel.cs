1f4e2112d589b07806b7c37cc6e9f7367f4deb07
JordanRoig <joro052@vidalibarraquer.net>
2025-05-02 20:51:50 +0200

    Agregar archivos de proyecto.

---
+++ CustomControl/RoundPanel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControl
+{
+    internal class RoundPanel : Panel
+    {
+        private int borderRadius = 20;
+
+        [Browsable(true)]
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius = value; Invalidate(); }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                Rectangle bounds = this.ClientRectangle;
+                int radius = borderRadius;
+
+                path.AddArc(bounds.X, bounds.Y, radius, radius, 180, 90);
+                path.AddArc(bounds.Right - radius, bounds.Y, radius, radius, 270, 90);
+                path.AddArc(bounds.Right - radius, bounds.Bottom - radius, radius, radius, 0, 90);
+                path.AddArc(bounds.X, bounds.Bottom - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+
+                this.Region = new Region(path);
+                g.FillPath(new SolidBrush(this.BackColor), path);
+            }
+        }
+    }
+}
